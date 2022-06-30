80efcc4072a7edab88ac8d1b97c5caf789c3564c
AlexBCSouza <alex.batista.91@gmail.com>
2022-06-30 16:16:22 -0300

    Test 2 - Casting Ajuste Part 2

--- src/estrutura_sequencial_variaveis/Test_2.java
+++ src/estrutura_sequencial_variaveis/Test_2.java
@@ -2,15 +2,12 @@ package estrutura_sequencial_variaveis;
 
 public class Test_2 {
     public static void main(String[] args) {
+        double a;
+        int b;
 
-    double b, B, h, area;
-
-    b = 15.0;
-    B = 20.0;
-    h = 5.0;
-
-    area = (b + B) / 2 * h;
-        System.out.println(area);
+        a = 5.0;
+        b = (int)a;
+        System.out.println(b);
 
 
     }
